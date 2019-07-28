66a87aa8ec25839820474c4c88bf0f3370ad610d
Odysseus <arwaterman@gmail.com>
2019-07-27 18:30:34 -0700

    testing yuuvis with jquery backend

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -3,6 +3,9 @@ import Previous from "./Previous";
 import Translate from "./Translate";
 import Compile from "./Compile";
 import Header from "./Header";
+
+import TestYuuvis from "../Components/testYuuvis";
+
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 export default props => {
   return (
@@ -12,6 +15,7 @@ export default props => {
         <Route path="/" exact component={Translate} />
         <Route path="/previous" exact component={Previous} />
         <Route path="/compile" exact component={Compile} />
+        <Route path="/test" exact component={TestYuuvis} />
       </Switch>
     </BrowserRouter>
   );
